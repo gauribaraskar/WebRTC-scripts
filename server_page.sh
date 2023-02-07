4a4925ffbaa0d936e4b08bc5727465a5ba4b0914
Gauri Baraskar <36883435+gauribaraskar@users.noreply.github.com>
2023-02-07 19:27:49 +0000

    Add files via upload

--- server_page.sh
+++ server_page.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+declare recordFile = checks
+echo '' > ${recordFile}
 while getopts n: flag
 do
     case "${flag}" in
@@ -6,15 +8,24 @@ do
     esac
 done
 # load the broadcast page in chrome
-for i in {1..5}
+for i in {1..numRuns}
 do
    echo "Starting run $i..."
    npm -i
    node server.js $i
    PID=$!
+   # 1. create a file on both 4.2 and 3.2 
+   # 2. viewer  should start its own google chrome
+   # 3. start the broadcaster
    google-chrome --headless --incognito http://localhost:5001/broadcast.html &
+   # 4. stop the node app
+    if [ ! -d "/proc/$PID" ]; then
+        sleep 2
+    fi
+   # 5. send a file to 3.2 to say done and then close the client tabs
+   'done' > ${recordFile}
    # change time here based on video ??
-   sleep 2m 30s
+   scp -P 2222 -i ~/.ssh/linksys ${recordFile} -c rebecca@192.168.3.2:~/Live-video-streaming-WebRTC/public
    # kill all chrome apps
    ps aux | awk '/chrome/ { print $2 } ' | xargs kill -9
    # kill the app
--- viewer_page.sh
+++ viewer_page.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
+declare recordFile = checks
+echo '' > ${recordFile}
+end=false
 while getopts n: flag
 do
     case "${flag}" in
         n) numRuns=${OPTARG};;
     esac
 done
-echo "num runs: $numRuns";
-for i in {1..5}
+for i in {1..numRuns}
 do
-   echo "Starting run $i..."
-   google-chrome --headless --incognito http://localhost:5001/index.html &
-   # change time here based on video ??
-   # ssh into server and start the broadcast script ??
-   sleep 2m 30s
-   ps aux | awk '/chrome/ { print $2 } ' | xargs kill -9
+    while :
+    do
+        mapfile < $recordFile
+        echo "${MAPFILE[@]}"
+        if [ "${MAPFILE[@]}" = "done" ]; then
+            ps aux | awk '/chrome/ { print $2 } ' | xargs kill -9
+            end = false
+            rm recordFile
+            break
+        if [ "${MAPFILE[@]}" = "" ] && [$end == false]; then
+            end = true
+            google-chrome --headless --incognito http://192.168.4.2:5001/index.html &
+        else
+            sleep 2
+    done
 done
